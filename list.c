67d7b8d3ff5bff9af819982b5d99c15a29f126ba
Arthur Rodrigues <arthurarp97@gmail.com>
2019-10-29 14:26:50 -0300

    adding structures to project

--- list.c
+++ list.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Pessoa
+{
+	char* nome;
+	int idade;
+	char* endereco;
+	char* email;
+	char* telefone;
+};
+
+struct Lista
+{
+	struct Pessoa pessoa;
+	struct Lista* prev;
+	struct Lista* next;
+};
+
+typedef struct Lista lista;
+
 int main(void)
 {
 
