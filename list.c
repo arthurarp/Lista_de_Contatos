ee1693e0986b528df1724b74020b8affa1a37d0b
Arthur Rodrigues <arthurarp97@gmail.com>
2020-03-31 03:10:20 -0300

    Modularizando e corrigindo erro de funcoes

--- list.c
+++ list.c
@@ -1,59 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "list.h"
 
-struct Person
-{
-	char* name;
-	int age;
-	char* adress;
-	char* email;
-	char* number;
-};
-
-struct List
-{
-	struct Person person;
-	struct List* prev;
-	struct List* next;
-};
-
-typedef struct List list;
-
-int is_empty(list** l)
+int is_empty(list* l)
 {
 	if(l)
-	{
-		if(*l){
-			return 1;
-		
-		else
-			return -2;
-	}
-	else
-		return -3;
+		return 1;
+
+	return 0;	
 }
 
-list** get_person_data()
+list* get_person_data()
 {
-	list** new = malloc(sizeof(list*)); 
+	list* new = (list *) malloc(sizeof(list)); 
 
-	printf("Please, tell us your name: ")
-	scanf("%s", new->name);
+	printf("Please, tell us your name: ");
+	scanf("%s", new->person.name);
 
 	printf("\nPlease, tell us your age: ");
-	scanf("%d", &new->age);
+	scanf("%d", &new->person.age);
 
 	return new;
 }
 
 
-void add_person_to_list(list** l)
+void add_person_to_list(list* l)
 {
-	list** new = get_person_data();
+	list* new = get_person_data();
 
 	if(is_empty(l))
 	{
-		*l = new;
+		l = new;
 		new->prev = new;
 		new->next = new;
 	}
@@ -61,12 +38,7 @@ void add_person_to_list(list** l)
 
 int main(void)
 {
-
-	list* l = malloc(sizeof(list *));
-
-	if (!l)
-		return -1;
-
+	list* l = (list *) malloc(sizeof(list));
 
 	
 
---
+++ list.h
@@ -0,0 +1,17 @@
+struct Person
+{
+	char name[100];
+	int age;
+	char adress[100];
+	char email;
+	char number[11];
+};
+
+struct List
+{
+	struct Person person;
+	struct List* prev;
+	struct List* next;
+};
+
+typedef struct List list;
